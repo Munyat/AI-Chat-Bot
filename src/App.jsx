7515fbb3816e2a8b5fd2ee2074ad5e6ed299ef2a
Brian Kipkirui Cheruiyot <briankipkiruimunyat@gmail.com>
2025-02-09 17:15:11 +0300

    completed app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,56 @@
 import { useState } from "react";
 import ChatBotApp from "./components/ChatBotApp";
 import ChatBotStart from "./components/ChatBotStart";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [isChatting, setIsChatting] = useState(false);
+  const [chats, setChats] = useState([]);
+  const [activeChat, setActiveChat] = useState([]);
 
-  function handleIsChatting() {
-    setIsChatting(!isChatting);
+  function handleStartChatting() {
+    setIsChatting(true);
+
+    if (chats.length === 0) {
+      createNewChat();
+    }
+  }
+  function handleGoBack() {
+    setIsChatting(false);
+  }
+
+  function createNewChat(initialMessage = "") {
+    const newChat = {
+      id: uuidv4(),
+      displayId: `Chat ${new Date().toLocaleDateString("en-GB")} ${new Date().toLocaleTimeString()}`,
+      messages: initialMessage
+        ? [
+            {
+              type: "prompt",
+              text: initialMessage,
+              timestamp: new Date().toLocaleTimeString(),
+            },
+          ]
+        : [],
+    };
+    const updatedChats = [newChat, ...chats];
+    setChats(updatedChats);
+    setActiveChat(newChat.id);
   }
 
   return (
     <div className="container">
       {isChatting ? (
-        <ChatBotApp onGoBack={handleIsChatting} />
+        <ChatBotApp
+          onGoBack={handleGoBack}
+          chats={chats}
+          setChats={setChats}
+          activeChat={activeChat}
+          setActiveChat={setActiveChat}
+          onNewChat={createNewChat}
+        />
       ) : (
-        <ChatBotStart onStartChat={handleIsChatting} />
+        <ChatBotStart onStartChat={handleStartChatting} />
       )}
     </div>
   );
--- src/components/ChatBotApp.jsx
+++ src/components/ChatBotApp.jsx
@@ -1,54 +1,214 @@
+import { useEffect, useRef, useState } from "react";
 import "./ChatBotApp.css";
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
-function ChatBotApp({ onGoBack }) {
+const ChatBotApp = ({
+  onGoBack,
+  chats,
+  setChats,
+  activeChat,
+  setActiveChat,
+  onNewChat,
+}) => {
+  const [inputValue, setInputValue] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+  const [showChatList, setShowChatList] = useState(false);
+  const chatEndRef = useRef(null);
+
+  useEffect(() => {
+    const activeChatObj = chats.find((chat) => chat.id === activeChat);
+    setMessages(activeChatObj ? activeChatObj.messages : []);
+  }, [activeChat, chats]);
+
+  useEffect(() => {
+    if (activeChat) {
+      const storedMessages = JSON.parse(localStorage.getItem(activeChat)) || [];
+      setMessages(storedMessages);
+    }
+  }, [activeChat]);
+
+  const handleEmojiSelect = (emoji) => {
+    setInputValue((prevInput) => prevInput + emoji.native);
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const sendMessage = async (e) => {
+    if (e) e.preventDefault();
+
+    if (inputValue.trim() === "") return;
+
+    const newMessage = {
+      type: "prompt",
+      text: inputValue,
+      timestamp: new Date().toLocaleTimeString(),
+    };
+
+    if (!activeChat) {
+      onNewChat(inputValue);
+      setInputValue("");
+    } else {
+      const updatedMessages = [...messages, newMessage];
+      setMessages(updatedMessages);
+      localStorage.setItem(activeChat, JSON.stringify(updatedMessages));
+      setInputValue("");
+
+      const updatedChats = chats.map((chat) => {
+        if (chat.id === activeChat) {
+          return { ...chat, messages: updatedMessages };
+        }
+        return chat;
+      });
+      setChats(updatedChats);
+      localStorage.setItem("chats", JSON.stringify(updatedChats));
+      setIsTyping(true);
+
+      const genAI = new GoogleGenerativeAI(
+        "AIzaSyAJCWeOcLFR41BYJsXPYwV1NSom3LfQXKQ"
+      );
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+      const prompt = inputValue;
+
+      const result = await model.generateContent(prompt);
+      console.log(result.response.text());
+
+      const chatResponse = result.response.text();
+
+      const newResponse = {
+        type: "response",
+        text: chatResponse,
+        timestamp: new Date().toLocaleTimeString(),
+      };
+
+      const updatedMessagesWithResponse = [...updatedMessages, newResponse];
+      setMessages(updatedMessagesWithResponse);
+      localStorage.setItem(
+        activeChat,
+        JSON.stringify(updatedMessagesWithResponse)
+      );
+      setIsTyping(false);
+
+      const updatedChatsWithResponse = chats.map((chat) => {
+        if (chat.id === activeChat) {
+          return { ...chat, messages: updatedMessagesWithResponse };
+        }
+        return chat;
+      });
+      setChats(updatedChatsWithResponse);
+      localStorage.setItem("chats", JSON.stringify(updatedChatsWithResponse));
+    }
+  };
+
+  // const handleKeyDown = (e) => {
+  //   if (e.key === "Enter") {
+  //     e.preventDefault();
+  //     sendMessage();
+  //   }
+  // };
+
+  const handleSelectChat = (id) => {
+    setActiveChat(id);
+  };
+
+  const handleDeleteChat = (id) => {
+    const updatedChats = chats.filter((chat) => chat.id !== id);
+    setChats(updatedChats);
+    localStorage.setItem("chats", JSON.stringify(updatedChats));
+    localStorage.removeItem(id);
+
+    if (id === activeChat) {
+      const newActiveChat = updatedChats.length > 0 ? updatedChats[0].id : null;
+      setActiveChat(newActiveChat);
+    }
+  };
+
+  useEffect(() => {
+    chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
   return (
     <div className="chat-app">
-      <div className="chat-list">
+      <div className={`chat-list ${showChatList ? "show" : ""}`}>
         <div className="chat-list-header">
           <h2>Chat List</h2>
-          <i className="bx bx-edit-alt new-chat"></i>
+          <i
+            className="bx bx-edit-alt new-chat"
+            onClick={() => onNewChat()}
+          ></i>
+          <i
+            className="bx bx-x-circle close-list"
+            onClick={() => setShowChatList(false)}
+          ></i>
         </div>
-        <div className="chat-list-item active">
-          <h4>Chat 20/07/2024 12:59:42 PM</h4>
-          <i className="bx bx-x-circle"></i>
-        </div>
-        <div className="chat-list-item">
-          <h4>Chat 20/07/2024 12:59:42 PM</h4>
-          <i className="bx bx-x-circle"></i>
-        </div>
-        <div className="chat-list-item">
-          <h4>Chat 20/07/2024 12:59:42 PM</h4>
-          <i className="bx bx-x-circle"></i>
+        <div className="scroll">
+          {chats.map((chat) => (
+            <div
+              key={chat.id}
+              className={`chat-list-item ${chat.id === activeChat ? "active" : ""}`}
+              onClick={() => handleSelectChat(chat.id)}
+            >
+              <h4>{chat.displayId}</h4>
+              <i
+                className="bx bx-x-circle"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleDeleteChat(chat.id);
+                }}
+              ></i>
+            </div>
+          ))}
         </div>
       </div>
       <div className="chat-window">
         <div className="chat-title">
           <h3>Chat with AI</h3>
-          <i onClick={onGoBack} className="bx bx-arrow-back arrow"></i>
+          <i className="bx bx-menu" onClick={() => setShowChatList(true)}></i>
+          <i className="bx bx-arrow-back arrow" onClick={onGoBack}></i>
         </div>
         <div className="chat">
-          <div className="prompt">
-            Hi how are you
-            <span>12:59:51</span>
-          </div>
-          <div className="response">
-            hello im just a computer program,how may i help you today
-            <span>12:59:51</span>
-          </div>
-          <div className="typing">Typing...</div>
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={msg.type === "prompt" ? "prompt" : "response"}
+            >
+              {msg.text} <span>{msg.timestamp}</span>
+            </div>
+          ))}
+          {isTyping && <div className="typing">Typing...</div>}
+          <div ref={chatEndRef}></div>
         </div>
-        <form className="msg-form">
-          <i className="fa-solid fa-face-smile emoji"></i>
+        <form className="msg-form" onSubmit={sendMessage}>
+          <i
+            className="fa-solid fa-face-smile emoji"
+            onClick={() => setShowEmojiPicker((prev) => !prev)}
+          ></i>
+          {showEmojiPicker && (
+            <div className="picker">
+              <Picker data={data} onEmojiSelect={handleEmojiSelect} />
+            </div>
+          )}
           <input
             type="text"
             className="msg-input"
-            placeholder="type a message..."
+            placeholder="Type a message..."
+            value={inputValue}
+            onChange={handleInputChange}
+            onFocus={() => setShowEmojiPicker(false)}
           />
-          <i className="fa-solid fa-paper-plane"></i>
+          <button type="submit" className="send-btn">
+            <i className="fa-solid fa-paper-plane"></i>
+          </button>
         </form>
       </div>
     </div>
   );
-}
+};
 
 export default ChatBotApp;
